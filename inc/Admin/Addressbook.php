0c2a1f5bf4177053ad4008a2332d5ba1c87bdec1
nazmunsakib <wp.coder3@gmail.com>
2022-04-11 11:02:32 +0600

    add sum function

---
+++ inc/Admin/Addressbook.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace myacademy\Admin;
+
+/**
+ *  This is address book class
+ */
+class Addressbook  {
+
+    /**
+     * The method for plugin page
+     */
+
+     function plugin_page(){
+         echo "hello form address book";
+     }
+
+}
--- inc/Admin/Menu.php
+++ inc/Admin/Menu.php
@@ -6,6 +6,7 @@ namespace myacademy\Admin;
  *The menu heading
  */
 class Menu {
+	
 	/**
 	 * Initialize the class
 	 */
@@ -19,11 +20,28 @@ class Menu {
 	 *Admin menu create
 	 */
 	function  academy_admin_menu(){
-		add_menu_page(__('Learn Academy', 'academy'), __('Academy', 'academy'), 'manage_options', 'learn-academy', [$this, 'plugin_page'], 'dashicons-welcome-learn-more');
+		$parent_sulg = 'learn-academy';
+		$manege_potions = 'manage_options';
+
+		add_menu_page(__('Learn Academy', 'academy'), __('Academy', 'academy'), $manege_potions,  $parent_sulg, [$this, 'address_book'], 'dashicons-welcome-learn-more');
+		add_submenu_page($parent_sulg, __('Address Book', 'academy'), __('Address Book', 'academy'), $manege_potions, $parent_sulg, [$this, 'address_book']);
+		add_submenu_page($parent_sulg, __('Settings', 'academy'), __('Settings', 'academy'), $manege_potions, 'academy-Settings', [$this, 'academy_Settings']);
 	}
 
-	function plugin_page(){
-		echo "Hello World";
+	/**
+	 * The address book callback function
+	 */
+	function address_book(){
+		$address_book = new Addressbook();
+		$address_book->plugin_page();
 	}
 
+	/**
+	 * The settigns page callback function
+	 */
+	function academy_Settings(){
+		echo "Hello WOrld";
+	}
+
+	
 }
